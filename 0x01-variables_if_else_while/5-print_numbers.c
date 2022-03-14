48a8dce7138be2f5387da3bd4cb90a43f332d954
asilanelson <asilanelson@gmail.com>
2022-03-13 17:36:57 -0700

    frustrating

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * main - Prints the numbers from 0 to 90
+ *
+ * Return: Always 0(Success)
+ */
 int main(void)
 {
+
+
+
 	for (int i = 0; i <= 9; i++)
 	{
 		printf("%d", i);
 	}
 	printf("\n");
+
+	printf("0123456789\n");
 return (0);
 }
